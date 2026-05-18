c031c37c7620eb8b1759527386efa2fd14daf836
Surya-ux-max <surprakas14@gmail.com>
2026-05-19 00:30:57 +0530

    refactor: comment out user input and output statements, retain age check logic

--- prj1/practice.py
+++ prj1/practice.py
@@ -10,8 +10,13 @@
 # print(name);
 # print(age);
 
-name = input("Enter your name: ");
-age = input("Enter your age: ");
+# name = input("Enter your name: ");
+# age = input("Enter your age: ");
 
-print("Your name is: " + name);
-print("Your age is: " + age);
+# print("Your name is: " + name);
+# print("Your age is: " + age);
+input = input("Enter a number: ");
+number = int(input);
+if(number == 18):
+    print("You are an adult."); 
+else:    print("You are not an adult.");
