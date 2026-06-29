ebc4a064ef44c935c170a6d35ea0cda4609dcfd5
Surya-ux-max <surprakas14@gmail.com>
2026-06-29 09:15:23 +0530

    Data Science - Reading from CSV

---
+++ Data_Science_practice/file1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = pd.read_csv(r'C:\Users\Suryaprakash\Downloads\New folder\Students_db.csv')
+
+print(data.head())
--- practice/file1.py
+++
@@ -1,4 +0,0 @@
-name = "Suryaprakash"
-print(name)
-
-// prints the name of user and type(name) will give the datatype of the variable
